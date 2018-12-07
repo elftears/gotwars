36b8bf2379a0e5dc190c83e56c75ee61d2917507
elftears <allann185@gmail.com>
2018-12-07 00:13:22 -0300

    a

---
+++ include/config.php
@@ -0,0 +1,31 @@
+<?php
+/*******************************************/
+/********* ARQUIVO DE CONFIGURAÇÃO *********/
+/********** Versão: Zapping Wars ***********/
+/*********** Por Caique Portela ************/
+/******* (No jogo: Felipe Medeiros) ********/
+/*******************************************/
+
+// Timezone
+date_default_timezone_set("America/Sao_Paulo");
+
+// Configurações do banco de dados
+$config['db_host'] = 'http://192.99.212.204'; // Host of your Database, probably Localhost
+$config['db_user'] = 'got'; // Database Username
+$config['db_pw'] = 'admin09'; // Database Password
+$config['db_name'] = 'db'; // Database Name
+
+// Acesso master ao painel administrativo
+$config['master_user'] = 'Admin';
+$config['master_pw'] = 'Zapping1234';
+
+// Configurações especiais...
+$config['name'] = '';
+$config['ano'] = '2014';
+$config['cdn'] = 'global_cdn';
+$config['forum'] = '#';
+$config['support'] = 'zapping_support/';
+$config['version'] = 'V0.1 DEVELOPMENT';
+$config['prefix'] = '';
+
+?>
